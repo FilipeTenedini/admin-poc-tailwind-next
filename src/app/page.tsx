6cc0de6d456b5888c0692748218a2ca953508b54
filipetenedini <tenedinifilipe@gmail.com>
2023-10-01 01:44:03 -0300

    feat: implements radix tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { SettingsTabs } from "@/components/SettingsTabs"
 import Image from "next/image"
 
 export default function Home() {
-  return <h1>home page</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingsTabs />
+    </>
+  )
 }
